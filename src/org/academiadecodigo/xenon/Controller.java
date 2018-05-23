3d0046e97c3e81f0201baac3ab2cccd8a98e41de
André Lopes <andre_j_t_lopes@hotmail.com>
2018-05-23 09:07:37 +0000

    Add Controller skeleton and modify gitignore

---
+++ src/org/academiadecodigo/xenon/Controller.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.xenon;
+
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+import org.academiadecodigo.xenon.world.gameobjects.Movable;
+
+public class Controller implements KeyboardHandler {
+
+    private Movable controllable;
+
+    public Controller(Movable movable) {
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+    }
+}
