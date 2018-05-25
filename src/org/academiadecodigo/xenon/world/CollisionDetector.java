37b854d2fa7d29b47023e5420182af2ed765a512
Nuno Paz <nunopaz@Air-de-Nuno-2.lan>
2018-05-25 21:32:56 +0100

    out

--- src/org/academiadecodigo/xenon/world/CollisionDetector.java
+++ src/org/academiadecodigo/xenon/world/CollisionDetector.java
@@ -3,8 +3,10 @@ package org.academiadecodigo.xenon.world;
 import java.util.List;
 
 import org.academiadecodigo.xenon.world.gameobjects.GameObject;
+import org.academiadecodigo.xenon.world.gameobjects.PlayerShip;
 
 public class CollisionDetector {
+    private PlayerShip playerShip;
     private List<GameObject> gameObjects;
 
     public CollisionDetector(GameObject gameObjects) {
@@ -16,10 +18,18 @@ public class CollisionDetector {
     }
 
     public void remove(GameObject gameObject) {
+        this.gameObjects.remove(gameObject);
         
     }
 
-    public void collide() {
+    /*public void collide(GameObject gameObject) {
+        for ( GameObject)
+        if(playerShip.overlaps(gameObject)){
+            remove(gameObject);
+            gameObject.isDestroyed();
+        }
+        if(gameObject.overlaps(playerShip)){
 
+        }*/
     }
-}
+
--- src/org/academiadecodigo/xenon/world/gameobjects/GameObject.java
+++ src/org/academiadecodigo/xenon/world/gameobjects/GameObject.java
@@ -1,6 +1,6 @@
 package org.academiadecodigo.xenon.world.gameobjects;
 
-import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 
 import org.academiadecodigo.xenon.world.GameMap;
 import org.academiadecodigo.xenon.world.Drawable;
