0c091741aefa0f1c46296f024c6bb800a17307ba
Code Cadet <codecadet@wozniak.local>
2018-05-27 18:01:28 +0000

    collide

--- src/org/academiadecodigo/xenon/world/CollisionDetector.java
+++ src/org/academiadecodigo/xenon/world/CollisionDetector.java
@@ -2,10 +2,14 @@ package org.academiadecodigo.xenon.world;
 
 import java.util.List;
 
+import org.academiadecodigo.xenon.world.gameobjects.EnemyShip;
 import org.academiadecodigo.xenon.world.gameobjects.GameObject;
+import org.academiadecodigo.xenon.world.gameobjects.PlayerShip;
+import org.academiadecodigo.xenon.world.gameobjects.Projectile;
 
 public class CollisionDetector {
     private List<GameObject> gameObjects;
+    private PlayerShip playerShip;
 
     public CollisionDetector(GameObject gameObjects) {
 
@@ -23,6 +27,23 @@ public class CollisionDetector {
     }
 
     public void collide() {
+        for(GameObject object1 : gameObjects){
+            for(GameObject object2: gameObjects){
+                if( object1 instanceof Projectile && object2 instanceof PlayerShip){
+                    object1.overlaps(object2);
+                    playerShip.hit();
+                }
+                if (object1 instanceof Projectile && object2 instanceof EnemyShip){
+                    object1.overlaps(object2);
+                    ((EnemyShip) object2).hit();
+                }
+                if(object1 instanceof EnemyShip && object2 instanceof PlayerShip){
+                    object1.overlaps(object2);
+                    ((PlayerShip) object2).hit();
+                    ((EnemyShip) object1).hit();
+                }
+            }
+        }
 
     }
 }
