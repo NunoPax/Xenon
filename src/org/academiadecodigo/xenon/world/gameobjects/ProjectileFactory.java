91635b55d58903b36da5fcc6580619f065a84cb8
Gabriela <gabi.tellus@gmail.com>
2018-05-28 21:09:31 +0100

    implementation of factories

--- src/org/academiadecodigo/xenon/world/gameobjects/ProjectileFactory.java
+++ src/org/academiadecodigo/xenon/world/gameobjects/ProjectileFactory.java
@@ -1,12 +1,56 @@
 package org.academiadecodigo.xenon.world.gameobjects;
 
+import org.academiadecodigo.xenon.world.GameMap;
+import org.academiadecodigo.xenon.world.gameobjects.Projectile;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class ProjectileFactory {
 
-    public static boolean canGetNewProjectile() {
+    private List<Projectile> projectiles;
+    private int maxProjectile;
+    private GameMap gameMap;
+
+
+    public ProjectileFactory(int maxProjectiles) {
+        this.maxProjectile = maxProjectiles;
+        this.projectiles = new LinkedList<>();
+    }
+
+    public void init() {
+
+        for (int i = 0; i < maxProjectile; i++) {
+
+            int x = 0;
+            int y = 0;
+            Projectile projectile = new Projectile(x, y, gameMap);
+            projectiles.add(projectile);
+        }
+
+    }
+
+    public Projectile get() {
+        if (projectiles.size() <= 0) {
+            return null;
+        }
+
+        return this.projectiles.remove(0);
+    }
+
+
+    public void offer(Projectile projectile) {
+        this.projectiles.add(projectile);
+    }
+
+
+    public boolean canGetNewProjectile() {
+        if (projectiles.size() > 0) {
+            return true;
+
+        }
+
         return false;
     }
 
-    public static Projectile getNewProjectile() {
-        return null;
-    }
 }
