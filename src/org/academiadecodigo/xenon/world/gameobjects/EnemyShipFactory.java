4149b3ac1c2669433baa68dc3de16411b4a917b4
André Lopes <andre_j_t_lopes@hotmail.com>
2018-05-28 22:28:11 +0000

    Fix some incompatibilities

--- src/org/academiadecodigo/xenon/world/gameobjects/EnemyShipFactory.java
+++ src/org/academiadecodigo/xenon/world/gameobjects/EnemyShipFactory.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.xenon.world.gameobjects;
 
 import org.academiadecodigo.xenon.world.CollisionDetector;
 import org.academiadecodigo.xenon.world.GameMap;
+import org.academiadecodigo.xenon.Game;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -13,13 +14,15 @@ public class EnemyShipFactory {
     private int maxShips;
     private CollisionDetector collisionDetector;
     private GameMap gameMap;
+    private Game game;
     //private List<newEnemyShip>;
 
-    public EnemyShipFactory(int maxShips, CollisionDetector collisionDetector, GameMap gameMap) {
+    public EnemyShipFactory(int maxShips, CollisionDetector collisionDetector, GameMap gameMap, Game game) {
         this.maxShips = maxShips;
         this.enemyShips = new LinkedList<>();
         this.collisionDetector = collisionDetector;
         this.gameMap = gameMap;
+        this.game = game;
     }
 
 
@@ -29,7 +32,7 @@ public class EnemyShipFactory {
 
             int x = GameMap.WIDTH - 10;
             int y = (int) (Math.random() * GameMap.HEIGHT - 10 - 10) + 10;
-            EnemyShip enemyShip = new EnemyShip(x, y, collisionDetector, gameMap);
+            EnemyShip enemyShip = new EnemyShip(x, y, collisionDetector, gameMap, game);
             enemyShips.add(enemyShip);
         }
     }
