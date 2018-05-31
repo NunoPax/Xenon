57a1c5f78449d19b02d378478e8ce731f466deee
André Lopes <andre_j_t_lopes@hotmail.com>
2018-05-31 21:04:48 +0000

    add extra enemies

--- src/org/academiadecodigo/xenon/world/gameobjects/SpaceShipType.java
+++ src/org/academiadecodigo/xenon/world/gameobjects/SpaceShipType.java
@@ -3,8 +3,10 @@ package org.academiadecodigo.xenon.world.gameobjects;
 public enum SpaceShipType {
 
     PLAYER(57, 61, "res/playerShip.png"),
-    ENEMY(60, 70, "res/enemyShip.png"),
-    ENEMY_BIGGER(70, 80, "res/medFighter.png");
+    ENEMY_A(60, 70, "res/enemyShip.png"),
+    ENEMY_B(70, 80, "res/medFighter.png"),
+    ENEMY_C(60, 99, "res/enemyC.png"),
+    ENEMY_D(90, 57, "res/enemyD.png");
 
     private int width;
     private int height;
