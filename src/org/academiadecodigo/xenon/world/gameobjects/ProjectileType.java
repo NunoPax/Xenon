19651043ab195095beba408591a7130e31fd435e
Code Cadet <codecadet@ritchie.local>
2018-05-31 18:19:52 +0000

    ProjectileType class creation

---
+++ src/org/academiadecodigo/xenon/world/gameobjects/ProjectileType.java
@@ -0,0 +1,33 @@
+package org.academiadecodigo.xenon.world.gameobjects;
+
+public enum ProjectileType {
+
+    STAR(22, 21, "res/star.png"),
+    CIRCLE(22, 22, "res/enemyProjectile");
+
+    private int width;
+    private int height;
+    private String path;
+
+
+    ProjectileType(int width, int height, String path) {
+        this.width = width;
+        this.height = height;
+        this.path = path;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public String getPad(){
+        return this.path;
+
+    }
+}
+
+
